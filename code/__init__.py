a29c36cff5f86f945640a60e7af45223d33764c6
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-04 17:03:28 -0500

    added code for reading prefrences from Preflib dataset

--- code/__init__.py
+++ code/__init__.py
@@ -1,1 +1,8 @@
+import csv
+
 print('testing')
+
+with open('D:\Lectures\Winter2015\CS886\Project\sushi3_preflib\sushi3_preflib\ED-00015-00000001.soc') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
