a25eeeeeb10c28d8be5fbd7aeba3283d8218ae05
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-06 13:44:23 -0500

    code added for graph plotting

--- ExecuteExperiment.py
+++ ExecuteExperiment.py
@@ -3,11 +3,18 @@ Created on Mar 5, 2015
 
 @author: Akshat
 '''
+import pandas as pd
+import matplotlib.pyplot as plt
 from pandas.core.frame import DataFrame
 
 # 'ebi', 'anago', 'maguro', 'ika', 'uni', 'sake', 'tamago', 'toro', 'tekka-maki', 'kappa-maki'
 
 def executeExp():
+    # Set some Pandas options
+    pd.set_option('display.notebook_repr_html', False)
+    pd.set_option('display.max_columns', 20)
+    pd.set_option('display.max_rows', 25)
+    
     pr = SushiPref('D:\Lectures\Winter2015\CS886\Project\sushi3_preflib\sushi3_preflib\ED-00015-00000001.soc')
     pr.loadUp()
 
@@ -32,6 +39,12 @@ def executeExp():
     fullPref = fullPref.transpose()
     fullPref.columns = ['ebi', 'anago', 'maguro', 'ika', 'uni', 'sake', 'tamago', 'toro', 'tekka-maki', 'kappa-maki']
     print(fullPref)
+    
+    
+    # For Graph Plot
+    fullPref.plot()
+    plt.show()
+    
 
 if __name__ == '__main__':
     from code.prefreaders import SushiPref
