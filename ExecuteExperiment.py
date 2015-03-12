b5ca33d16abd94ecbba48e6d5a48641ec8b698e3
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-12 16:55:03 -0400

    moved some code out of the main module and into the core modules

--- ExecuteExperiment.py
+++ ExecuteExperiment.py
@@ -4,7 +4,7 @@ Created on Mar 5, 2015
 @author: Akshat
 '''
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pandas.core.frame import DataFrame
 
 # 'ebi', 'anago', 'maguro', 'ika', 'uni', 'sake', 'tamago', 'toro', 'tekka-maki', 'kappa-maki'
@@ -19,32 +19,14 @@ def executeExp():
     pr.loadUp()
 
     pu = SushiPrefUtil(pr)
+    pref_matrix = pu.gen_pref_matrix() 
     
-    rank1 = pu.getRankCount('01')
-    rank2 = pu.getRankCount('02')
-    rank3 = pu.getRankCount('03')
-    rank4 = pu.getRankCount('04')
-    rank5 = pu.getRankCount('05')
-    rank6 = pu.getRankCount('06')
-    rank7 = pu.getRankCount('07')
-    rank8 = pu.getRankCount('08')
-    rank9 = pu.getRankCount('09')
-    rank10 = pu.getRankCount('10')
     
-    fullPref = DataFrame(dict(r1=rank1, r2=rank2, r3=rank3, r4=rank4, r5=rank5, r6=rank6, r7=rank7, r8=rank8, r9=rank9, r10=rank10))
-    fullPref.index.name = 'SushiName'
+    print(pref_matrix)
     
-    print('RESULTS...')
-    
-    fullPref = fullPref.transpose()
-    fullPref.columns = ['ebi', 'anago', 'maguro', 'ika', 'uni', 'sake', 'tamago', 'toro', 'tekka-maki', 'kappa-maki']
-    print(fullPref)
-    
-    '''
     # For Graph Plot
-    fullPref.plot()
+    pref_matrix.plot()
     plt.show()
-    '''
 
 if __name__ == '__main__':
     from code.prefreaders import SushiPref
