5bacbc17316c28d79ed6cd418639589a240b5e9d
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-03-16 17:08:30 -0400

    - removed print statement in prefUtil.py - completed fix for issue reported by @p8peng

--- code/prefUtil.py
+++ code/prefUtil.py
@@ -55,7 +55,5 @@ class SushiPrefUtil(object):
         fullPref = fullPref.sort(['indexNumber'], ascending=[True])
         fullPref = fullPref.drop('indexNumber', 1)
         
-        print(fullPref)
-        
         return fullPref
         
