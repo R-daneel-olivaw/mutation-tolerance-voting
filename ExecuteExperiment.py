bd79688d02b48d6f1f8ad6c27493ab312ef737f0
Jie Peng <j29peng@uwaterloo.ca>
2015-03-23 12:23:40 -0400

    Examples of how to use this mutation

--- ExecuteExperiment.py
+++ ExecuteExperiment.py
@@ -8,6 +8,7 @@ from code.sf.sf_irv import ImplIRV
 from code.sf.sf_plurality import ImplPlurality
 from code.sf.sf_plurality_at_large import ImplPluralityAtLarge
 from code.pref_matrix_plot import PrefPlotter
+from MutationFunc.RandomSwap import Mutation
 
 # 'ebi', 'anago', 'maguro', 'ika', 'uni', 'sake', 'tamago', 'toro', 'tekka-maki', 'kappa-maki'
 
@@ -21,6 +22,12 @@ def executeExp():
     pc = ImplIRV(pref_plotter.raw_pref)
     pc = ImplPlurality(pref_plotter.raw_pref)
     pc = ImplPluralityAtLarge(pref_plotter.raw_pref)
-
+    
+    #print(pref_plotter.raw_pref.getDf())
+    for index, row in pref_plotter.raw_pref.getDf().iterrows():
+        randomswap=Mutation(row,0.5)
+        randomswap.normalNoise()
+        print(row)
+    
 if __name__ == '__main__':
     executeExp()
