a335c322f58e04a6e72e7eff12b914cb7d8c8dd0
Akshat Kumar Vaish <akshatvaish.public@gmail.com>
2015-04-10 14:54:31 -0400

    for #2

---
+++ recursive_execution/controller.py
@@ -0,0 +1,17 @@
+'''
+Created on Apr 10, 2015
+
+@author: Akshat
+'''
+
+class RecursiveExecutionController(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
